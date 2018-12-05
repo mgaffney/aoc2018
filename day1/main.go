4ca8fa59a22c7d0a1f0f22b0bc41f946a86db51b
Michael Gaffney <mike@gaffney.cc>
2018-12-05 08:49:10 -0500

    Day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -8,17 +8,32 @@ import (
 )
 
 func main() {
-	var total int
+	var cs []int
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		a, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "converting number:", err)
+			os.Exit(1)
 		}
-		total = total + a
+		cs = append(cs, a)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		os.Exit(1)
 	}
-	fmt.Println(total)
+
+	var freq int
+	freqs := make(map[int]bool)
+	for {
+		for _, a := range cs {
+			freq = freq + a
+			if _, ok := freqs[freq]; !ok {
+				freqs[freq] = true
+			} else {
+				fmt.Println("second time: ", freq)
+				os.Exit(0)
+			}
+		}
+	}
 }
